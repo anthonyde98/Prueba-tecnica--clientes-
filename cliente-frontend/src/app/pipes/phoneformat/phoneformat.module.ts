a08d6457f2122e2ec1a1600acaad1d50865c9571
anthonyde98 <anthonyde98@gmail.com>
2022-10-21 20:35:38 -0400

    Error en modulo arreglado.

--- cliente-frontend/src/app/pipes/phoneformat/phoneformat.module.ts
+++ cliente-frontend/src/app/pipes/phoneformat/phoneformat.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { IonicModule } from '@ionic/angular';
 import { PhoneFormatPipe } from './phoneformat.pipe'; 
 
 @NgModule({
   imports: [
-    CommonModule,
-    IonicModule,
+    CommonModule
   ],
   declarations: [PhoneFormatPipe],
   exports: [PhoneFormatPipe]
